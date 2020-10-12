ca7530978e3404f27a16ff718c08fa1b59dea65e
shahedc <shahedc@gmail.com>
2020-10-12 15:59:22 -0400

    initial version with webapi and console projects

---
+++ myconsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
